e3a5af531d40c51180fde6163f42033029780555
Juan Maldonado <maldonadojsm@gmail.com>
2020-02-26 18:01:11 -0500

    Add files via upload

---
+++ makeVim.sh
@@ -0,0 +1,10 @@
+sudo yum -y install cmake
+cp vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/universal-ctags/ctags.git
+cd ctags
+./autogen.sh
+./configure
+make
+sudo make install
+
